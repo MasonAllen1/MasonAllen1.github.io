9ce86d5d84acfd1bf6807a0940b6c6c739a4b7bb
MasonAllen1 <masallen1220@gmail.com>
2024-08-13 18:21:38 +0000

    added h2

---
+++ dom.js
@@ -0,0 +1,8 @@
+let colorbutton =document.querySelector("#please")
+
+function changebackgroundcolor(){
+    document.body.style.backgroundColor = 'black'
+} 
+
+colorbutton.addEventListener("click", changebackgroundcolor)
+    
